a04495a1ac032bed0e66813dfd4d36ea9519f29c
root <root@raspberrypi.(none)>
2014-09-26 19:47:53 +0000

    final touches

--- src/MyRobot.cpp
+++ src/MyRobot.cpp
@@ -1,1 +1,46 @@
 /* This is where you define your robot */
+
+class MyRobot : BasicRobot 
+{
+	
+	DriveSystem MyDrive;
+	LiftSystem MyLift;
+	IntakeSystem CubeIntake;
+
+	void robotInit();
+	void disabledInit();
+	void disabledLoop();
+	void autoInit();
+	void mainAutoCode();
+	void driverInit();
+	void mainDriverCode();
+
+}
+
+void MyRobot::robotInit()
+{
+}
+
+void MyRobot::disabledInit()
+{
+}
+
+void MyRobot::disabledLoop()
+{
+}
+
+void MyRobot::autoInit()
+{
+}
+
+void MyRobot::mainAutoCode()
+{
+}
+
+void MyRobot::driverInit()
+{
+}
+
+void MyRobot::mainDriverCode()
+{
+}
